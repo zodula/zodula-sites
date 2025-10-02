12d8a86e315d33a5a8e2ed0473daec92ef57e35d
ppenter <jarupak.sri@gmail.com>
2025-10-03 06:08:20 +0700

    Update layout.tsx

--- sites/docs/src/app/docs/[[...slug]]/layout.tsx
+++ sites/docs/src/app/docs/[[...slug]]/layout.tsx
@@ -1,7 +1,7 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
 import { source } from '@/lib/source';
-
+ 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions()}>
